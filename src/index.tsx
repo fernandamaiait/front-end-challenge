9c19e37edfca13c833e7be0b44b29fe257783b02
fernandamaiabettha <fernanda.maia@bettha.com>
2023-08-12 16:51:58 -0300

    refactor: cleaning code

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import HomePage from './pages/home-page/HomePage';
+//import HomePage from './pages/home-page/HomePage';
 import TopBar from './components/TopBar';
+import MovieDetailPage from './pages/movie-detail-page/MovieDetailPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <TopBar />
-    <HomePage />
+    {/* <HomePage /> */}
+    <MovieDetailPage />
   </React.StrictMode>
 );
--- src/pages/home-page/components/CategoriesList.tsx
+++ src/pages/home-page/components/CategoriesList.tsx
@@ -81,7 +81,7 @@ export default function CategoriesList() {
   // ];
   return (
     <div className="bg-secondary flex flex-col w-full px-4  py-10 sm:py-[84px] justify-center sm:justify-start sm:items-center ">
-      <p className="font-roboto text-on-secondary text-[24px] leading-[56px] font-bold sm:text-center sm:text-[48px]">
+      <p className="font-roboto text-on-secondary text-[24px] leading-[56px] font-bold sm:text-center sm:text-[48px] sm:w-[781px]">
         Milhões de filmes, séries e pessoas para descobrir. Explore já.
       </p>
       {/* <p className="text-sm font-bold text-on-secondary self-start sm:self-center mt-9 sm:mt-10">
